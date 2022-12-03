cde3a65e1faba0a60d1384192a2fefca67528a28
felipepassos09 <felipe.passos.09@gmail.com>
2022-12-03 15:31:47 -0300

    setup MySql connection

---
+++ ProjectMVC/ProjectMVC/Data/ProjectMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjectMVC.Models;
+
+namespace ProjectMVC.Data
+{
+    public class ProjectMVCContext : DbContext
+    {
+        public ProjectMVCContext (DbContextOptions<ProjectMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Department> Department { get; set; }
+    }
+}
--- ProjectMVC/ProjectMVC/Program.cs
+++ ProjectMVC/ProjectMVC/Program.cs
@@ -1,8 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using ProjectMVC.Data;
 var builder = WebApplication.CreateBuilder(args);
+//builder.Services.AddDbContext<ProjectMVCContext>(options =>
+//    options.UseMySql(ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("ProjectMVCContext")), builder => builder.MigrationsAssembly("ProjectMVC")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+
+var MySqlConnection = builder.Configuration.GetConnectionString("ProjectMVC");
+
+builder.Services.AddDbContextPool<ProjectMVCContext>(options =>
+    options.UseMySql(MySqlConnection,ServerVersion.AutoDetect(MySqlConnection)));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
